5788ac535ec1b2a970c8e9bcf65a316d9b008c01
jou <jou.42.rio@gmail.com>
2024-03-19 19:15:10 -0300

    dots x start fixed, need to fix y start. edge.wid math reverted (not bugging)

--- set_fdf.c
+++ set_fdf.c
@@ -35,7 +35,8 @@ t_edge	*set_edge(t_map map)
 		return (NULL);
 	edge->height = WIN_HEIGHT / height_len;
 	edge->size = edge->height * 2;
-	edge->width = get_edge_width(edge->size);
+	// edge->width = get_edge_width(edge->size);
+	edge->width = edge->height * sqrt(3);
 	map_width = (map.columns + map.rows) * edge->width;
 	if (map_width > WIN_WIDTH)
 	{
@@ -43,7 +44,8 @@ t_edge	*set_edge(t_map map)
 		map_width = WIN_WIDTH;
 		edge->height = map_height / height_len;
 		edge->size = edge->height * 2;
-		edge->width = get_edge_width(edge->size);
+		// edge->width = get_edge_width(edge->size);
+		edge->width = edge->height * sqrt(3);
 	}
 	return (edge);
 }
@@ -75,8 +77,8 @@ t_dot	**set_dots(t_map map, t_edge edge)
 	col = 0;
 	dots = malloc_dots(map);
 	// dots[row][col] = get_start(map, edge);
-	dots[row][col].x = (WIN_WIDTH - ((map.columns + map.rows) * edge.width)) / 2;
-	dots[row][col].y = WIN_HEIGHT - (map.rows * edge.height);
+	dots[row][col].x = (WIN_WIDTH - ((map.columns + map.rows - 2) * edge.width)) / 2;
+	dots[row][col].y = WIN_HEIGHT - ((map.rows - 1) * edge.height);
 	col++;
 	while (row < map.rows)
 	{
